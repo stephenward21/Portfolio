6fda7b9bc6fa3b83e6eab82e6e6a901b86fc5dbe
Stephen Ward <stephen.ward21@gmail.com>
2017-08-14 17:20:33 -0400

    slider changes

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+	//  
 	$('.about').click(function(){
 		event.preventDefault();
 		$('html, body').animate({scrollTop: $(".about-section").offset().top}, 1500);
@@ -22,6 +23,24 @@ $(document).ready(function(){
 
 	$('.opening').typeIt({
      strings: 'My name is Stephen Ward...I am a full stack web developer!  Please feel free to stay awhile and look around!'
+	});
+
+	$('.slider-for').slick({
+	  slidesToShow: 1,
+	  slidesToScroll: 1,
+	  arrows: false,
+	  fade: true,
+	  asNavFor: '.slider-nav'
+	});
+
+
+	$('.slider-nav').slick({
+	  slidesToShow: 3,
+	  slidesToScroll: 1,
+	  asNavFor: '.slider-for',
+	  arrows: true,
+	  centerMode: true,
+	  focusOnSelect: true
 	});
 
 	$('.contact').click(function(){
