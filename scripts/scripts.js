ba3b3c3014348b414d092a97ce3f3e237396f474
Stephen Ward <stephen.ward21@gmail.com>
2017-08-14 21:16:16 -0400

    slick carousel

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -22,26 +22,29 @@ $(document).ready(function(){
 	// });
 
 	$('.opening').typeIt({
-     strings: 'My name is Stephen Ward...I am a full stack web developer!  Please feel free to stay awhile and look around!'
+     strings: 'My name is Stephen Ward...I\'m a full stack web developer!  Please feel free to stay awhile and look around!'
 	});
 
 	$('.slider-for').slick({
 	  slidesToShow: 1,
 	  slidesToScroll: 1,
-	  arrows: false,
-	  fade: true,
-	  asNavFor: '.slider-nav'
+	  autoplay: true,
+	  autoplaySpeed: 3000,
+	  arrows: false
+	  // fade: true,
+	  // asNavFor: '.slider-nav',
+	  // initialSlide: 1
 	});
 
 
-	$('.slider-nav').slick({
-	  slidesToShow: 3,
-	  slidesToScroll: 1,
-	  asNavFor: '.slider-for',
-	  arrows: true,
-	  centerMode: true,
-	  focusOnSelect: true
-	});
+	// $('.slider-nav').slick({
+	//   slidesToShow: 3,
+	//   slidesToScroll: 1,
+	//   asNavFor: '.slider-for',
+	//   arrows: true,
+	//   centerMode: true,
+	//   focusOnSelect: true
+	// });
 
 	$('.contact').click(function(){
 		$('#myModal').modal();
